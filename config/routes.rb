75e8096a847f84bb1fba05be6428d3136f9d6c7a
satoshitodaka <satoshitodaka0705@gmail.com>
2023-04-22 16:08:01 +0900

    add Profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :publishers
   resources :books, only: %i[show destroy]
+  resource :profile, only: %i[show edit update]
 end

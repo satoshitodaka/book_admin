aca780f2fb403838114ff85dc6c48a8f8f0eba1f
satoshitodaka <satoshitodaka0705@gmail.com>
2023-04-22 12:18:30 +0900

    validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authers, dependent: :destroy
   has_many :authers, through: :book_authers
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal: 0 }
+  validate do |book|
+    if book.name.include?('exercise')
+      book.errors[:name] << "I don't like exercise"
+    end
+  end
 end
